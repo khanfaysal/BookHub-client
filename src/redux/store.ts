f8a1326b3c0550442f3315d4807554a13223238c
khan faysal <mdfaysalkhancse@gmail.com>
2023-07-18 23:30:35 +0600

    hook code modify

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -7,7 +7,7 @@ import userReducer from './features/user/userSlice';
 
 const storedLastUpdatedBooks = JSON.parse(localStorage.getItem('lastUpdatedBooks') || '[]');
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     book: bookReducer,
     user: userReducer,
@@ -26,4 +26,7 @@ export const store = configureStore({
     },
   },
 });
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
 export default store;
